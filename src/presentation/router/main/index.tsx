069b7d95ec5055bb330588480d18a14dcfeecea3
Eric Silva <ericsilvaccp@gmail.com>
2022-04-19 15:11:23 -0300

    feat: config splash screen in IOS

--- src/presentation/router/main/index.tsx
+++ src/presentation/router/main/index.tsx
@@ -1,4 +1,12 @@
 import { MainProvider } from '@presentation/provider/main'
-import React from 'react'
+import React, { useEffect } from 'react'
+import ReactNativeSplashScreen from 'react-native-splash-screen'
 
-export const MainRouter = () => <MainProvider />
+export const MainRouter = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      ReactNativeSplashScreen.hide()
+    }, 5000)
+  }, [])
+  return <MainProvider />
+}
