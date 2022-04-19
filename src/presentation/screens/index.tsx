772afcd5b8bb9a74c2268b2a4bdb13721addc812
Eric Silva <ericsilvaccp@gmail.com>
2022-04-18 23:00:01 -0300

    feat: create screen component

---
+++ src/presentation/screens/index.tsx
@@ -0,0 +1,9 @@
+import { ButtonPrimary } from '@presentation/atomic/molecules/button/primary'
+import React from 'react'
+import { Container } from './style'
+
+export const Screen = () => (
+  <Container>
+    <ButtonPrimary />
+  </Container>
+)
